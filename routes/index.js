0533779fb7d7509c256ae9445507d62412165bc2
eltonc88 <eltonchan@cal.berkeley.edu>
2015-10-06 23:33:14 -0700

    add get index

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,8 @@ var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/to
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-router.post('/api/v1/todos', function(req, res) {
 
+router.post('/api/v1/todos', function(req, res) {
   var results = [];
 
   // Grab data from http request
@@ -42,5 +42,34 @@ router.post('/api/v1/todos', function(req, res) {
     });
   });
 });
+
+router.get('/api/v1/todos', function(req, res) {
+    var results = [];
+
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+          return res.status(500).json({ success: false, data: err});
+        }
+
+        // SQL Query > Select Data
+        var query = client.query("SELECT * FROM items ORDER BY id ASC;");
+
+        // Stream results back one row at a time
+        query.on('row', function(row) {
+            results.push(row);
+        });
+
+        // After all data is returned, close connection and return results
+        query.on('end', function() {
+            done();
+            return res.json(results);
+        });
+    });
+});
+
 
 module.exports = router;
