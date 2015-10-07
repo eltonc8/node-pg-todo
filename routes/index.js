fc4c2facd439ab224a2b569049e8cfaeae0ed59f
eltonc88 <eltonchan@cal.berkeley.edu>
2015-10-06 23:41:12 -0700

    add delete route

--- routes/index.js
+++ routes/index.js
@@ -108,5 +108,39 @@ router.put('/api/v1/todos/:todo_id', function(req, res) {
   });
 });
 
+router.delete('/api/v1/todos/:todo_id', function(req, res) {
+  var results = [];
+
+  // Grab data from the URL parameters
+  var id = req.params.todo_id;
+
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, function(err, client, done) {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({ success: false, data: err});
+    }
+
+    // SQL Query > Delete Data
+    client.query("DELETE FROM items WHERE id=($1)", [id]);
+
+    // SQL Query > Select Data
+    var query = client.query("SELECT * FROM items ORDER BY id ASC");
+
+    // Stream results back one row at a time
+    query.on('row', function(row) {
+      results.push(row);
+    });
+
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
 
 module.exports = router;
