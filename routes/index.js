bfd3bd7ccb834c392c0526c6ae7f8600c05a09f7
eltonc88 <eltonchan@cal.berkeley.edu>
2015-10-08 23:10:11 -0700

    add basic display of items

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
-// var path = require('path'); require(path.join(__dirname, '../', '../', 'config'));
+var path = require('path');
+// require(path.join(__dirname, '../', '../', 'config'));
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname, '../views', 'index.html'));
 });
 
 router.post('/api/v1/todos', function(req, res) {
