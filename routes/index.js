48929dcd90d4b9b36331add37a4d9fa3fc7e2659
eltonc88 <eltonchan@cal.berkeley.edu>
2015-10-28 23:44:39 -0700

    refactor error handling

--- routes/index.js
+++ routes/index.js
@@ -13,17 +13,19 @@ router.get('/', function(req, res, next) {
 router.post('/api/v1/todos', function(req, res) {
   var results = [];
 
+  var handleError = function() {
+    done();
+    console.log(err);
+    return res.status(500).json({ success: false, data: err});
+  }
+
   // Grab data from http request
   var data = {text: req.body.text, complete: false};
 
   // Get a Postgres client from the connection pool
   pg.connect(connectionString, function(err, client, done) {
     // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({ success: false, data: err});
-    }
+    if(err) handleError();
 
     // SQL Query > Insert Data
     client.query("INSERT INTO items(text, complete) values($1, $2)", [data.text, data.complete]);
@@ -50,11 +52,7 @@ router.get('/api/v1/todos', function(req, res) {
   // Get a Postgres client from the connection pool
   pg.connect(connectionString, function(err, client, done) {
     // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({ success: false, data: err});
-    }
+    if(err) handleError();
 
     // SQL Query > Select Data
     var query = client.query("SELECT * FROM items ORDER BY id ASC;");
@@ -84,11 +82,7 @@ router.put('/api/v1/todos/:todo_id', function(req, res) {
   // Get a Postgres client from the connection pool
   pg.connect(connectionString, function(err, client, done) {
     // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).send(json({ success: false, data: err}));
-    }
+    if(err) handleError();
 
     // SQL Query > Update Data
     client.query("UPDATE items SET text=($1), complete=($2) WHERE id=($3)", [data.text, data.complete, id]);
@@ -118,11 +112,7 @@ router.delete('/api/v1/todos/:todo_id', function(req, res) {
   // Get a Postgres client from the connection pool
   pg.connect(connectionString, function(err, client, done) {
     // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({ success: false, data: err});
-    }
+    if(err) handleError();
 
     // SQL Query > Delete Data
     client.query("DELETE FROM items WHERE id=($1)", [id]);
